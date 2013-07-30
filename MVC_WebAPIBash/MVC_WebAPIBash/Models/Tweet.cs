1220a8a2784682e3d0d5dc9fc89409356ab37ac8
lukew <lukew@microsoft.com>
2013-07-30 14:07:22 -0700

    tweet.cs was updated, and this didn't scaffold, bug filed

--- MVC_WebAPIBash/MVC_WebAPIBash/Models/Tweet.cs
+++ MVC_WebAPIBash/MVC_WebAPIBash/Models/Tweet.cs
@@ -8,6 +8,7 @@ namespace MVC_WebAPIBash.Models
     public class Tweet
     {
         public virtual int Id { get; set; }
+        public virtual string Title { get; set; }
         public virtual string Body { get; set; }
         public virtual User User { get; set; }
         public virtual int UserId { get; set; }
